03f9e27de45ce7b6f73fd8632d752e3ae795462f
YuliiaMoholivets <veronikaanddiana1@gmail.com>
2022-12-10 22:15:00 +0200

    10-12

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,68 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
 
-console.log(galleryItems);
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryContainer.addEventListener('click', onGalleryContainerClick);
+
+function createGalleryMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+  <div class="gallery__item">
+    <a class="gallery__link" target="_self" title="Click to enlarge" rel="nofollow, noreferrer" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${description}"
+        />
+    </a>
+  </div>
+  `;
+    })
+    .join('');
+}
+function onGalleryContainerClick(event) {
+  event.preventDefault();
+
+  const isGalleryImage = event.target.classList.contains('gallery__image');
+
+  if (!isGalleryImage) {
+    return;
+  }
+
+  ligthboxGallery();
+
+  const openedImage = document.querySelector('.is-opened');
+  openedImage.src = event.target.dataset.source;
+  openedImage.alt = event.target.alt;
+}
+
+function ligthboxGallery() {
+  const ligthbox = basicLightbox.create(
+    `
+    <img class="is-opened" src="https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg" width="1280">
+`,
+    {
+      onShow: () => {
+        window.addEventListener('keydown', onEscapeKey);
+      },
+      onClose: () => {
+        window.removeEventListener('keydown', onEscapeKey);
+      },
+    }
+  );
+
+  function onEscapeKey(event) {
+    const ESC_KEY_CODE = 'Escape';
+    console.log(`esc`);
+
+    if (event.code === ESC_KEY_CODE) {
+      ligthbox.close();
+    }
+  }
+
+  ligthbox.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,34 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// markup function
+function createGalleryMarkup(images) {
+  return images
+    .map(({ preview, original, description }) => {
+      return `
+      <a class="gallery__item"
+      target="_self"
+      rel="nofollow, noreferrer"
+      title="Click to enlarge" 
+      href="${original}">
+        <img class="gallery__image" 
+        src="${preview}" 
+        alt="${description}" />
+      </a>`;
+    })
+    .join('');
+}
+
+// lightbox
+var lightbox = new SimpleLightbox('.gallery a', {
+  //   options
+  captionDelay: 250,
+  captionSelector: 'img',
+  captionsData: 'alt',
+});
