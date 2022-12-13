2c72efac07c233a635984a369ff48dc92f27eea9
YuliiaMoholivets <veronikaanddiana1@gmail.com>
2022-12-13 19:22:31 +0200

    13-12

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,68 +1,32 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 const galleryContainer = document.querySelector('.gallery');
-const galleryMarkup = createGalleryMarkup(galleryItems);
 
-galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
-galleryContainer.addEventListener('click', onGalleryContainerClick);
+const galleryImages = galleryItems.map(({ preview, original, description }) => {
+   return `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`;
+});
+galleryContainer .insertAdjacentHTML('beforeend', galleryImages.join(''));
 
-function createGalleryMarkup(images) {
-  return images
-    .map(({ preview, original, description }) => {
-      return `
-  <div class="gallery__item">
-    <a class="gallery__link" target="_self" title="Click to enlarge" rel="nofollow, noreferrer" href="${original}">
-        <img
-        class="gallery__image"
-        src="${preview}"
-        data-source="${original}"
-        alt="${description}"
-        />
-    </a>
-  </div>
-  `;
-    })
-    .join('');
-}
-function onGalleryContainerClick(event) {
-  event.preventDefault();
+galleryContainer .addEventListener(
+   'click',
+   event => {
+      if (event.target.closest('.gallery__link')) {
+         event.preventDefault();
+         const instance = basicLightbox.create(
+            `<img src="${event.target.closest('img').dataset.source}" width="800" height="600">`
+         );
+         instance.show();
+      }
+   },
+   false
+);
 
-  const isGalleryImage = event.target.classList.contains('gallery__image');
-
-  if (!isGalleryImage) {
-    return;
-  }
-
-  ligthboxGallery();
-
-  const openedImage = document.querySelector('.is-opened');
-  openedImage.src = event.target.dataset.source;
-  openedImage.alt = event.target.alt;
-}
-
-function ligthboxGallery() {
-  const ligthbox = basicLightbox.create(
-    `
-    <img class="is-opened" src="https://cdn.pixabay.com/photo/2019/05/16/09/47/beach-4206785_1280.jpg" width="1280">
-`,
-    {
-      onShow: () => {
-        window.addEventListener('keydown', onEscapeKey);
-      },
-      onClose: () => {
-        window.removeEventListener('keydown', onEscapeKey);
-      },
-    }
-  );
-
-  function onEscapeKey(event) {
-    const ESC_KEY_CODE = 'Escape';
-    console.log(`esc`);
-
-    if (event.code === ESC_KEY_CODE) {
-      ligthbox.close();
-    }
-  }
-
-  ligthbox.show();
-}
+document.addEventListener('keyup', event => {
+   if (!document.querySelector('.basicLightbox')) {
+      return;
+   }
+   const basicLightboxEL = document.querySelector('.basicLightbox');
+   if ((event.code === 'Escape') & (basicLightboxEL !== null)) {
+      basicLightboxEL.remove();
+   }
+});
